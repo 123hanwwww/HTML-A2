2bfcdd02d8ac5ba715bacca04c5cb63873fd53d7
123hanwwww <3241657311@qq.com>
2026-04-07 21:53:45 +0800

    修复页面按钮无法点击的问题，添加必要的路由模块导入

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { InventoryService, InventoryItem } from '../inventory.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
